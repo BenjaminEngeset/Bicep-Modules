2ac0eb584ab855d3fc239b4e59e30bd701c64e18
Benjamin Engeset <99641908+bengeset96@users.noreply.github.com>
2022-02-19 13:26:28 +0100

    Delete scripts/ACR directory

--- scripts/ACR/Validate-ACRModules.ps1
+++
@@ -1,4 +0,0 @@
-$modules = Get-ChildItem $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY -Include *.bicep -Recurse # Only include the files with type of .bicep. 
-foreach ($module in $modules) {
-    bicep build $module.FullName
-}
