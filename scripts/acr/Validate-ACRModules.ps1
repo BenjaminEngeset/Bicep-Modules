a3c626658d5dff719992a4b217a236d2a72661c2
Benjamin Engeset <beneng@hotmail.no>
2022-02-19 17:57:34 +0100

    tune up script

--- scripts/acr/Validate-ACRModules.ps1
+++ scripts/acr/Validate-ACRModules.ps1
@@ -1,4 +1,4 @@
-$modules = Get-ChildItem $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY -Include *.bicep -Recurse # Only include the files with type of .bicep. 
+$modules = Get-ChildItem $ENV:BUILD_ARTIFACTSTAGINGDIRECTORY -Include *.bicep -Recurse # only include the files with type of .bicep
 foreach ($module in $modules) {
     bicep build $module.FullName
 }
